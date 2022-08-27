ec3c3edaf534ef526c5631f51fe518880eba04c6
Spyros Paterakis <spyridon.paterakis@outlook.com.gr>
2022-08-27 17:49:23 +0300

    introduce controller for each model

---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
