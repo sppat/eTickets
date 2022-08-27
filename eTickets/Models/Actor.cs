a038ab41f23f8d45825639587e256d8fae188558
Spyros Paterakis <spyridon.paterakis@outlook.com.gr>
2022-08-27 07:41:39 +0300

    add new models

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models;
+
+public class Actor
+{
+    [Key]
+    public int Id { get; set; }
+    public string ProfilePictureUrl { get; set; }
+    public string FullName { get; set; }
+    public string Bio { get; set; }
+}
